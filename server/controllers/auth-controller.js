01a3b5a98a1c22b4f1babc6114bc813a9a8f2ede
khanmustafaraza <khanmustafarazaec1994@gmail.com>
2025-09-11 13:31:00 +0530

    add one user to database

--- server/controllers/auth-controller.js
+++ server/controllers/auth-controller.js
@@ -1,3 +1,5 @@
+const User = require("../models/user-model");
+
 const home = async (req,res)=>{
     try {
         res.status(200).send(`this is index page`)
@@ -8,8 +10,32 @@ const home = async (req,res)=>{
 
 const register = async(req,res)=>{
     try {
-        console.log(req.body)
-        res.status(200).send(req.body)
+        const {username,email,password} = req.body;
+        userExist = await User.findOne({email});
+        if(userExist){
+            return res.status(400).json({
+                msg:"User Alreday Exsist"
+            })
+        }
+         const userObj = new User({
+            username,
+            email,
+            password
+        });
+
+        const savedUser = await userObj.save(); // <- fix here
+    //     const userObj ={
+    //         username,
+    //         email,password
+    //     }
+    //   const savedUser = await User.create(userObj)
+    
+      if(savedUser){
+         res.status(200).json({
+            msg:"Account Created Successfully"
+         })
+      }
+       
     } catch (error) {
         console.log(error)
     }
--- server/utlis/db.js
+++ server/utlis/db.js
@@ -1,6 +1,6 @@
 const mongoose = require("mongoose")
 
-const URI = "mongodb://127.0.0.1:27017/mern-app"
+const URI = process.env.MONGO_URI
 
 
 const connectDB = async() =>{
