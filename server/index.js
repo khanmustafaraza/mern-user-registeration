faa74ff89c0ebf60d9185e26b2625e6886673f4e
khanmustafaraza <khanmustafarazaec1994@gmail.com>
2025-09-11 16:27:46 +0530

    again test the .env

--- server/index.js
+++ server/index.js
@@ -1,37 +1,25 @@
-require("dotenv").config()
+require("dotenv").config();
 const express = require("express");
 
 const app = express();
 
-const  authrouter = require("./router/auth-router");
+const authrouter = require("./router/auth-router");
 const connectDB = require("./utlis/db");
 
-app.use(express.json())
+app.use(express.json());
 
-// this is the best method
-app.get("/",(resq,res)=>{
-    res.status(200).send("<h1>Welcome to the home page</h1>")
-})
-
-// todo all middleware 
-app.use("/api/auth",authrouter)
-
-
-
-
-
-
-
-
-
-
-
-
-// creating the sever
-
-const PORT =5000;
-connectDB().then(()=>{
-    app.listen(PORT,()=>{
-    console.log(`Server is running at http://localhost:${PORT}`)
+app.get("/", (resq, res) => {
+  res.status(200).send("<h1>Welcome to the home page</h1>");
 });
-})
+
+// todo all middleware
+app.use("/api/auth", authrouter);
+
+// creating the sever request at home
+
+const PORT = 5000;
+connectDB().then(() => {
+  app.listen(PORT, () => {
+    console.log(`Server is running at http://localhost:${PORT}`);
+  });
+});
